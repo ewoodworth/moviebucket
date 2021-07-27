e843f0c271bef5b0cb53b5afeb9f982a5bd76423
Erin <software@open5g.com>
2021-07-27 16:13:37 -0700

    Comment cleanup

--- datamodel.py
+++ datamodel.py
@@ -14,23 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-
-class Transaction(db.Model):
-    __tablename__ = 'transactions'
-
-    id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String())
-    last_name = db.Column(db.String())
-    amount = db.Column(db.Float)
-
-    def __init__(self, first_name: str, last_name: str, amount: float):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.amount = amount
-
-    def __repr__(self):
-        return f'{self.first_name} {self.last_name} spent {self.amount}'
-
 class Movie(db.Model):
     """Movies"""
 
